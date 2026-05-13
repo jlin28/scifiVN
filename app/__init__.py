ab0db8fdb11e13979b36ee2a810b9b072c8a7900
Joyce Lin <joycel78@nycstudents.net>
2026-05-13 13:00:34 -0400

    Added route for each page

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,25 @@ app = Flask(__name__)
 app.secret_key = "vnnnnn"
 
 @app.route("/", methods=["GET", "POST"])
-def login():
-    return render_template('login.html')
+def start():
+    return render_template('start.html')
+    
+@app.route("/select", methods=["GET", "POST"])
+def select():
+    return render_template('select.html')
+    
+@app.route("/map", methods=["GET", "POST"])
+def map():
+    return render_template('map.html')
+    
+@app.route("/room", methods=["GET", "POST"])
+def room():
+    return render_template('room.html')
 
+@app.route("/end", methods=["GET", "POST"])
+def end():
+    return render_template('end.html')
+    
 if __name__ == "__main__":
     app.debug = False
     app.run()
