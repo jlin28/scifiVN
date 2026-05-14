1abc69f3927e3a43a05ac5e72abe512f3b4fdc07
Joyce Lin <jlin60@stuy.edu>
2026-05-14 13:19:10 -0400

    bleh

--- app/__init__.py
+++ app/__init__.py
@@ -7,15 +7,15 @@ app.secret_key = "vnnnnn"
 @app.route("/", methods=["GET", "POST"])
 def start():
     return render_template('start.html')
-    
+
 @app.route("/select", methods=["GET", "POST"])
 def select():
     return render_template('select.html')
-    
+
 @app.route("/map", methods=["GET", "POST"])
 def map():
     return render_template('map.html')
-    
+
 @app.route("/room", methods=["GET", "POST"])
 def room():
     return render_template('room.html')
@@ -23,7 +23,7 @@ def room():
 @app.route("/end", methods=["GET", "POST"])
 def end():
     return render_template('end.html')
-    
+
 if __name__ == "__main__":
-    app.debug = False
+    app.debug = True
     app.run()
